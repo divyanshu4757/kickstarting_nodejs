8e390d9bfc172877837870deb577279a6caf7b42
divyanshu4757 <guptadivyanshu400@gmail.com>
2023-08-04 09:51:31 +0530

    express 1st assignment

--- first.js
+++ first.js
@@ -1,13 +1,26 @@
 
-const http = require('http');
+
+const express = require("express");
 
 
-const server = http.createServer((req, res) => {
-    
-    res.write('hello world');
-    res.end();
+const app  = express();
+
+app.use((req,res,next)=>{
+               
+    console.log('in the middle ware');
+    next();//allows request to continue to next middleware
+
+   
 })
 
 
+app.use((req,res,next)=>{
+               
+    console.log('in another middle ware');
+    res.send('<h1>hello</h1>');
+})
 
-server.listen(3000);
+
+;
+
+app.listen(9000);
